df271c9f5a214878b91a0f953e39087073d40a00
Hunter Buxton <hunter.buxton@protonmail.com>
2021-06-01 21:05:40 -0600

    changed var names on the RequestList component

--- proj1-client/src/components/RequestList.jsx
+++ proj1-client/src/components/RequestList.jsx
@@ -4,46 +4,46 @@ import Request from './Request';
 
 function RequestList() {
 
-  const [todos, setTodos] = useState([{ id: 1, text: "amount: 500, status: pending, date: 05/03/2020 " },
+  const [requests, setRequests] = useState([{ id: 1, text: "amount: 500, status: pending, date: 05/03/2020 " },
   { id: 2, text: "amount: 4, status: pending, date: 05/03/2020 " },
   { id: 3, text: "amount: 30, status: complete, date: 05/02/2020 " },
   { id: 4, text: "amount: 200, status: denied, date: 05/01/2020 " },
   { id: 5, text: "amount: 1111, status: complete, date: 05/01/2020 " }]);
 
-  const addTodo = todo => {
-    if (!todo.text || /^\s*$/.test(todo.text)) {
+  const addRequest = request => {
+    if (!request.text || /^\s*$/.test(request.text)) {
       return;
     }
 // 'amount: ' + todo + ' status: pending, date: 05/19/2021 '
-    todo.text = 'amount: ' + todo.text + ' status: pending, date: 05/19/2021 '
-    const newTodos = [todo, ...todos];
+    request.text = 'amount: ' + request.text + ' status: pending, date: 05/19/2021 '
+    const newTodos = [request, ...requests];
 
-    setTodos(newTodos);
-    console.log(...todos);
+    setRequests(newTodos);
+    console.log(...requests);
   };
 
-  const updateTodo = (todoId, newValue) => {
+  const updateRequest = (todoId, newValue) => {
     if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
 
-    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    setRequests(prev => prev.map(item => (item.id === todoId ? newValue : item)));
   };
 
-  const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo.id !== id);
+  const removeRequest = id => {
+    const removedArr = [...requests].filter(todo => todo.id !== id);
 
-    setTodos(removedArr);
+    setRequests(removedArr);
   };
 
-  const completeTodo = id => {
-    let updatedTodos = todos.map(todo => {
+  const completeRequest = id => {
+    let updatedTodos = requests.map(todo => {
       if (todo.id === id) {
         todo.isComplete = !todo.isComplete;
       }
       return todo;
     });
-    setTodos(updatedTodos);
+    setRequests(updatedTodos);
   };
 
 //   addTodo("amount: 500, status: pending, date: 05/04/2020 ");
@@ -53,13 +53,13 @@ function RequestList() {
         <h1>Reimbursements</h1>
      
      <Request
-       todos={todos}
-       completeTodo={completeTodo}
-       removeTodo={removeTodo}
-       updateTodo={updateTodo}
+       todos={requests}
+       completeTodo={completeRequest}
+       removeTodo={removeRequest}
+       updateTodo={updateRequest}
      />
 
-      <RequestForm onSubmit={addTodo} />
+      <RequestForm onSubmit={addRequest} />
       </div>
   );
 }
